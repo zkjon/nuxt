58bf8b62f1041c674d9e1a9f40a89c1971c64ca7
zkj0n <focusyts@gmail.com>
2025-08-27 23:42:52 +0200

    refactor(components): simplify DemoButton styling and update Footer content
    - Remove inline Tailwind classes from DemoButton and prepare for external styling
    - Replace license link in Footer with DemoButton component for GitHub stars

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,12 +1,9 @@
 <template>
   <footer class="w-fit mx-auto px-10 py-5 border-t border-white/10">
+    
     <p>
-      <a
-        href="https://github.com/zkjon/nuxt-template/blob/master/LICENSE"
-        target="_blank"
-        class="hover:underline"
-        >MIT LICESE</a
-      >
+      <DemoButton href="https://github.com/zkjon/nuxt-template"> ⭐Star me on github </DemoButton>
+      
       © 2025 zkjon
     </p>
   </footer>
