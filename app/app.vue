160e55fc04c9ed546e53c16df2cc2a73febc54cd
zkj0n <focusyts@gmail.com>
2025-08-27 22:25:15 +0200

    refactor(app): update main layout with custom content and styling
    Replace default Nuxt welcome page with custom main layout featuring centered content and proper styling

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main class="h-screen flex items-center justify-center flex-col">
+    <h1 class="text-3xl font-bold">This is my personal website</h1>
+    <TODO></TODO>
+  </main>
 </template>
