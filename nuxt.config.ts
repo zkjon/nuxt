7d8cedcc889768ba97f666b045e0060bd6a95851
zkj0n <focusyts@gmail.com>
2025-08-27 21:56:22 +0200

    build: add path aliases configuration
    Configure path aliases in tsconfig.json and nuxt.config.ts to simplify imports using "@/" prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+    alias: {
+      "@": path.resolve("./"),
+    },
   vite: {
     plugins: [tailwindcss()],
   },
